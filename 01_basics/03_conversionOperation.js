dddff1cf0f0307497dd87930e0ff8f5f028b8231
debapriya_dutta <110217632+duttadebapriya@users.noreply.github.com>
2025-04-01 06:43:13 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,5 +23,39 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 36
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber))
+//console.log(stringNumber);
+// console.log(typeof(stringNumber))
+
+
+// ******************* Operations *********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+let str1 = "hello, "
+let str2 = "ravi!"
+let str3 = str1+str2
+//console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+ 2 +2);
+// console.log(1+ 2 +"2");
+
+// console.log(+true); //1
+
+// let num1,num2,num3
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;  //postfix
+++gameCounter;  //prefix
+console.log(gameCounter);
