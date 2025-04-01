44e93b450954f5a73b37af803dc59c0a237ae427
debapriya_dutta <110217632+duttadebapriya@users.noreply.github.com>
2025-04-01 04:37:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";  //treat all JS code as newer version
+
+//  alert("Hello")   we are using nodejs, not browser.
+
+// code readablility should be high.
+
+// datatypes
+// number -> 2^53
+// bigInt
+// string = " "
+// boolean -> true/false
+// null -> standalone value
+// undefined
+// symbol -> unique
+
+
+
+// object
+
+console.log(typeof("ravi"))
+console.log(typeof(null))    //object
+console.log(typeof(undefined))    //undefined
