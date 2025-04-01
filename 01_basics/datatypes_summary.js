1e3c28bec0095ee76e5a2a907cea52855f767d57
debapriya_dutta <110217632+duttadebapriya@users.noreply.github.com>
2025-04-01 11:25:49 +0000

    datatypes

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -26,3 +26,28 @@ console.log(typeof (heroes));
 console.log(typeof myObj);
 console.log(typeof myFunction);
 console.log(typeof anotherId);
+
+
+
+// **************************************************************************************
+
+// Stack(Primitive)
+//Heap(Non-primitive)
+
+let myYoutubeName = "Musicoholic"
+let anotherName = myYoutubeName
+anotherName = "chaiaurcode"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "dd@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
