05408f3ef60dbcee645df5930465924f671b2970
debapriya_dutta <110217632+duttadebapriya@users.noreply.github.com>
2025-04-01 05:12:44 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "dd"
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+
+/* "33" -> 33
+ "33abc" -> NaN
+ true -> 1, false -> 0
+*/
+
+
+let isLoggedIn = ""
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+/*1 -> true, 0 -> false
+ "" -> false, "dd" -> true
+*/
+
+
+let someNumber = 36
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber))
