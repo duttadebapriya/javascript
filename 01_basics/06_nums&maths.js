740430493a899ff6b1ae6a1b58caad61294d991a
debapriya_dutta <110217632+duttadebapriya@users.noreply.github.com>
2025-04-02 04:02:06 +0000

    math and num

---
+++ 01_basics/06_nums&maths.js
@@ -0,0 +1,36 @@
+const score = 400
+
+const balance = new Number(1000)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// console.log(typeof balance);
+
+const num = 36.6767
+// console.log(num.toPrecision(2));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// *******************MATHS**************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.333));
+// console.log(Math.ceil(3.42));
+// console.log(Math.floor(4.2));
+
+console.log(Math.random());   // values between 0 & 1
+console.log((Math.random()*10) +1);
+console.log(Math.floor((Math.random()*10)+1)); 
+
+
+const min = 10
+const max = 20
+
+console.log(Math.random()*(max-min+1));
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
